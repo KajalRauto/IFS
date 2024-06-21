c7cdc1182146859d419e1e8bc77424298961fdbf
KajalRauto <kajalrauto2410@gmail.com>
2024-06-21 10:48:11 +0530

    added proper spacing

--- src/Components/HomePage.jsx
+++ src/Components/HomePage.jsx
@@ -1,8 +1,6 @@
-import { useContext, useEffect } from "react";
 import AboutUs from "./AboutUs";
 import Home from "./Home";
 import Products from "./Products";
-import { StoreList } from "../Store/store-list";
 
 function HomePage() {
   return <div>
